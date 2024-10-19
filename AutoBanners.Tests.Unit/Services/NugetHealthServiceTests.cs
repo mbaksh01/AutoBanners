6e7a74212d843e34e0d91d4ac92a5cd6f5136fde
Moe <mbaksh@redev.co.uk>
2024-10-19 21:46:36 +0100

    Fixed test

--- AutoBanners.Tests.Unit/Services/NugetHealthServiceTests.cs
+++ AutoBanners.Tests.Unit/Services/NugetHealthServiceTests.cs
@@ -101,7 +101,7 @@ public class NugetHealthServiceTests
         var response = await healthService.GetHealthAsync(nugetConfiguration);
 
         // Assert
-        response.Should().Be(HealthStatus.Unknown);
+        response.Should().Be(HealthStatus.Unhealthy);
         mockHandler.GetMatchCount(mockedRequest).Should().Be(1);
     }
 }
